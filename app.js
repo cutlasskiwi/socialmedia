1cff57780599541cd9b7496a8cb35ddf58674b62
cutlasskiwi <ahhsandra@gmail.com>
2020-10-23 11:53:36 +0200

    Modul 4: Social Media
    Small update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Display posts:
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((res) => res.json())
   .then((res) => {
@@ -15,7 +16,7 @@ fetch("https://jsonplaceholder.typicode.com/posts")
           '<button class="comment-button" data-postid="' +
           post.id +
           '">Read comments</button>' +
-          '<button class="author-button" data-userid="' +
+          '<button class="author-button" id="author" data-userid="' +
           post.userId +
           '">Author info</button>' +
           "</div>"
@@ -24,3 +25,51 @@ fetch("https://jsonplaceholder.typicode.com/posts")
       .join("");
     document.getElementById("posts").innerHTML = postsHtml;
   });
+
+// Author info:
+fetch("https://jsonplaceholder.typicode.com/users/")
+  .then((res) => res.json())
+  .then((res) => {
+    users = res;
+    userHtml = res
+      .map((user) => {
+        return (
+          '<div class="user">' +
+          "<h2>" +
+          user.username +
+          "</h2>" +
+          "<div>" +
+          user.name +
+          "</div>" +
+          "<div>" +
+          user.email +
+          "</div>" +
+          "<div>" +
+          user.phone +
+          "</div>" +
+          "<div>" +
+          user.company +
+          "</div>" +
+          "</div>"
+        );
+      })
+      .join("");
+    const authorbtn = document.getElementById("author");
+    authorbtn.addEventListener(
+      "click",
+      function () {
+        document.getElementById("user-container").innerHTML = userHtml;
+      },
+      false
+    );
+  });
+//   <!--
+//   <div class="user">
+//       <h2>Author</h2>
+//       <div>Leanne Graham</div>
+//       <div>Sincere@april.biz</div>
+//       <div>Phone: 1-770-736-8031 x56442</div>
+//       <br>
+//       <div>Company: Romaguera-Crona</div>
+//   </div>
+//   -->
